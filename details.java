461cc358e2b698e1fdf531c8b92df9c70af1ce94
kavya <kavyareddyaddela@gmail.com>
2025-02-28 10:13:14 +0530

    added from desktop

---
+++ details.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class student_detials
+{
+String name,roll no,mail,address,gender,blood group,dep,sec,religion,caste,hobbies;
+int aadhar no,contact no,DOD,year;
+}
+class parent_details
+{
+String Parent name,ocupation,qualification;
+int Parent contact no,monthly income;
+}
+class acadamic_details 
+{
+String internship,placement;
+int CGPA,supplies,attendence;
+}
+public class main
+{
+
+}
+
